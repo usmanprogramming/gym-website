6a73293feca19db21077a8f4709a474717941e4b
Usman Siddiqui <siddiquishoaib515@gmail.com>
2025-03-28 16:59:06 +0500

    nothing changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
-import Blog from "./pages/blog/Blog";
+import Blog from "./pages/blog/Blog"; 
 import Trainers from "./pages/trainers/Trainers";
 import About from "./pages/about/About";
 
 function App() {
   return (
     <div className="app">
-      {/* <Header />
+      <Header />
       <Home />
       <Blog />
-      <Trainers /> */}
+      <Trainers />
       <About />
     </div>
   );
